528f94c5e787684d8e3f370ecabad9d8cbe51470
Christine Dodrill <xena@yolo-swag.com>
2015-03-05 17:59:53 -0800

    Run gofmt

--- codes.go
+++ codes.go
@@ -5,27 +5,27 @@ type Color int
 
 // Color codes that mIRC uses. Names taken from http://www.mirc.com/colors.html.
 const (
-  ColorWhite Color = iota
-  ColorBlack
-  ColorBlue
-  ColorGreen
-  ColorLightRed
-  ColorBrown
-  ColorPurple
-  ColorOrange
-  ColorYellow
-  ColorLightGreen
-  ColorCyan
-  ColorLightCyan
-  ColorLightBlue
-  ColorPink
-  ColorGray
-  ColorLightGray
+	ColorWhite Color = iota
+	ColorBlack
+	ColorBlue
+	ColorGreen
+	ColorLightRed
+	ColorBrown
+	ColorPurple
+	ColorOrange
+	ColorYellow
+	ColorLightGreen
+	ColorCyan
+	ColorLightCyan
+	ColorLightBlue
+	ColorPink
+	ColorGray
+	ColorLightGray
 )
 
 //go:generate stringer -type=Color
 
 // The delimiter for color codes
 const (
-  ColorCodeDelim = '\x03'
-  )
+	ColorCodeDelim = '\x03'
+)
