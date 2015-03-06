a7a6c8de929b3fcc936bd571234a78f257a91c03
Christine Dodrill <xena@yolo-swag.com>
2015-03-05 18:35:44 -0800

    Add NewColoredText

--- coloredtext.go
+++ coloredtext.go
@@ -1,6 +1,13 @@
 package color
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrWrongColorNumber = errors.New("Wrong number of colors")
+)
 
 // ColoredText is a section of text that is colored with mIRC color codes.
 type ColoredText struct {
@@ -15,4 +22,42 @@ func (c ColoredText) String() string {
 	}
 
 	return fmt.Sprintf("%c%d%s%c", ColorCodeDelim, c.Foreground, c.Content, ColorCodeDelim)
+}
+
+// NewColoredText makes a ColoredText with given colors and a content or returns
+// a blank ColoredText and an error.
+func NewColoredText(colors []Color, content string) (c ColoredText, err error) {
+	switch len(colors) {
+	case 0:
+		c.Background = None
+		c.Foreground = None
+		c.Content = content
+
+	case 1:
+		c.Background = None
+		if colors[0].Validate() {
+			c.Foreground = colors[0]
+		} else {
+			return ColoredText{}, ErrBadColor
+		}
+
+	case 2:
+		if colors[0].Validate() {
+			c.Foreground = colors[0]
+		} else {
+			return ColoredText{}, ErrBadColor
+		}
+
+		if colors[1].Validate() {
+			c.Background = colors[1]
+		} else {
+			return ColoredText{}, ErrBadColor
+		}
+
+	default:
+		return ColoredText{}, ErrWrongColorNumber
+
+	}
+
+	return
 }
