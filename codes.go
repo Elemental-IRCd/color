755fcfa7627ab6f4934a6109d8f9eb0e7abfb5a1
Christine Dodrill <xena@yolo-swag.com>
2015-03-05 17:31:37 -0800

    Add color code definitions

---
+++ codes.go
@@ -0,0 +1,31 @@
+package color
+
+// A color code that mIRC defines
+type Color int
+
+// Color codes that mIRC uses. Names taken from http://www.mirc.com/colors.html.
+const (
+  ColorWhite Color = iota
+  ColorBlack
+  ColorBlue
+  ColorGreen
+  ColorLightRed
+  ColorBrown
+  ColorPurple
+  ColorOrange
+  ColorYellow
+  ColorLightGreen
+  ColorCyan
+  ColorLightCyan
+  ColorLightBlue
+  ColorPink
+  ColorGray
+  ColorLightGray
+)
+
+//go:generate stringer -type=Color
+
+// The delimiter for color codes
+const (
+  ColorCodeDelim = '\x03'
+  )
