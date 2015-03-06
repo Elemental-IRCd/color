a064873b672092dd3f76b1bd64b2589a884d219c
Christine Dodrill <xena@yolo-swag.com>
2015-03-06 08:24:58 -0800

    Document Color.Validate()

--- color.go
+++ color.go
@@ -5,6 +5,7 @@ type Color int
 
 //go:generate stringer -type=Color
 
+// Validate makes sure that the recieving color makes sense.
 func (c Color) Validate() bool {
 	return c >= None && c < (LightGray+1)
 }
