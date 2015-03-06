07554ce169661a96bd196158b0990e2c24983adb
Christine Dodrill <xena@yolo-swag.com>
2015-03-05 18:34:28 -0800

    Add error message for a wrong color code

--- codes.go
+++ codes.go
@@ -1,5 +1,7 @@
 package color
 
+import "errors"
+
 // Color codes that mIRC uses. Names taken from http://www.mirc.com/colors.html.
 const (
 	White Color = iota
@@ -19,6 +21,10 @@ const (
 	Gray
 	LightGray
 	None Color = -1 // No color
+)
+
+var (
+	ErrBadColor = errors.New("Bad color code")
 )
 
 // The delimiter for color codes
